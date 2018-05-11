e71577117dcd97cace89f37687a2636e3f8bee6c
TomWesterhout <westerhout_tom@hotmail.com>
2018-05-11 02:24:18 +0200

    Added methods to the todo-data service that make RESTful requests to the API service.

--- src/app/todo-data.service.ts
+++ src/app/todo-data.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Todo } from './todo';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs/Observable';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class TodoDataService {
 
-  constructor() { }
+  constructor(private api: ApiService) { }
+
+  // Makes a post request to the /todos path of the API.
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.api.createTodo(todo);
+  }
+
+  // Makes a delete request to the /todos/:id path of the API.
+  deleteTodoById(todoId: number): Observable<Todo> {
+    return this.api.deleteTodoById(todoId);
+  }
+
+  // Makes a put/patch request to the /todos/:id path of the API
+  updateTodo(todo: Todo): Observable<Todo> {
+    return this.api.updateTodo(todo);
+  }
+
+  // Makes a get request to the /todos path of the API.
+  getAllTodos(): Observable<Todo[]> {
+    return this.api.getAllTodos();
+  }
+
+  // Makes a get request to the /todos/:id path of the API.
+  getTodoById(todoId: number): Observable<Todo> {
+    return this.api.getTodoById(todoId);
+  }
+
+  // Toggles the complete property of the todo object given as an argument.
+  toggleTodoComplete(todo: Todo) {
+    todo.complete = !todo.complete;
+    return this.api.updateTodo(todo);
+  }
+
 }
