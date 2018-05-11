f0f123a6f1f7312382f6e842b53f51e8f29d77a4
TomWesterhout <westerhout_tom@hotmail.com>
2018-05-11 02:42:50 +0200

    Added imports and methods to the app component. The methods call their corresponding methods in the todo-data service, which eventually lead to the htpp requests being made in the api service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
 import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
   todos: Todo[] = [];
 
-  
+  constructor(private todoDataService: TodoDataService) {}
+
+  ngOnInit() {
+    this.todoDataService.getAllTodos()
+      .subscribe((todos) => {
+        this.todos = todos;
+      });
+  }
+
+  onAddTodo(todo) {
+    if (this.duplicateOrEmpty(todo)) {
+      return;
+    }
+    this.todoDataService
+      .addTodo(todo)
+      .subscribe(
+        (newTodo) => {
+          this.todos = this.todos.concat(newTodo);
+        }
+      );
+  }
+
+  onToggleTodoComplete(todo) {
+    this.todoDataService
+      .toggleTodoComplete(todo)
+      .subscribe(
+        (updatedTodo) => {
+          todo = updatedTodo;
+        }
+      );
+  }
+
+  onRemoveTodo(todo) {
+    this.todoDataService
+      .deleteTodoById(todo.id)
+      .subscribe(
+        (_) => {
+          this.todos = this.todos.filter((t) => t.id !== todo.id);
+        }
+      );
+  }
+
+  duplicateOrEmpty(todo) {
+    for(let i = 0; i < this.todos.length; i++) {
+      if (todo.title === "" || todo.title === this.todos[i].title) {
+        return true;
+      }
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
 import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
 import { TodoListFooterComponent } from './todo-list-footer/todo-list-footer.component';
+
+import { TodoDataService } from './todo-data.service';
 import { ApiService } from './api.service';
 
 @NgModule({
@@ -20,9 +24,10 @@ import { ApiService } from './api.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
-  providers: [ApiService],
+  providers: [TodoDataService, ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
